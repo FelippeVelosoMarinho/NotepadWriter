edfdd9a4aabd267c28cce9bd3831cda0d9eefaa5
Felippe Veloso <60450622+FelippeVelosoMarinho@users.noreply.github.com>
2021-02-14 20:59:08 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 
         public static void main(String[] args) throws IOException {
             ManipularArquivo mf = new ManipularArquivo();
-            String path = "C:/Users/Helio/IdeaProjects/TrabalhoDeCarnaval/src/file.txt";
+            String path = "/file.txt";
 
             mf.escritor(path);
             mf.leitor(path);
